195e2267203da4768afd886da0c58b1d95e2fc97
shadowfax <theshadowfaxproject@gmail.com>
2025-03-26 23:14:54 -0700

    Reapply "open-webui-64 Remove model-config for users from open-webui page."
    This reverts commit a5375b3183f58e43da339fb90e6afdd5689e65fa.

--- src/lib/components/chat/Navbar.svelte
+++ src/lib/components/chat/Navbar.svelte
@@ -24,6 +24,7 @@
 	import UserMenu from '$lib/components/layout/Sidebar/UserMenu.svelte';
 	import MenuLines from '../icons/MenuLines.svelte';
 	import AdjustmentsHorizontal from '../icons/AdjustmentsHorizontal.svelte';
+	import {WEBUI_BASE_URL} from "$lib/constants";
 
 	import PencilSquare from '../icons/PencilSquare.svelte';
 
@@ -64,8 +65,21 @@
 					aria-label="Toggle Sidebar"
 				>
 					<div class=" m-auto self-center">
-						<MenuLines />
-					</div>
+						<svg
+ 							xmlns="http://www.w3.org/2000/svg"
+ 							fill="none"
+ 							viewBox="0 0 24 24"
+ 							stroke="currentColor"
+ 							stroke-width="2.5"
+ 							class="size-3.5"
+ 						>
+ 						<path
+ 							stroke-linecap="round"
+ 							stroke-linejoin="round"
+ 							d="m8.25 4.5 7.5 7.5-7.5 7.5"
+ 						/>
+ 						</svg>
+  					</div>
 				</button>
 			</div>
 
@@ -146,7 +160,7 @@
 					</Tooltip>
 				{/if}
 
-				<Tooltip content={$i18n.t('New Chat')}>
+				<Tooltip content={$i18n.t('New Gift Chat')}>
 					<button
 						id="new-chat-button"
 						class=" flex {$showSidebar
@@ -155,7 +169,7 @@
 						on:click={() => {
 							initNewChat();
 						}}
-						aria-label="New Chat"
+						aria-label="New Gift Chat"
 					>
 						<div class=" m-auto self-center">
 							<PencilSquare className=" size-5" strokeWidth="2" />
