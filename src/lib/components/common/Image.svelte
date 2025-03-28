a4bd742a5ec1ddd121985833331929d904e1005e
shadowfax <theshadowfaxproject@gmail.com>
2025-03-28 11:58:02 -0700

    magicbox-app-163 Modify product-image display to use caption instead of adding additional  row-line

--- src/lib/components/common/Image.svelte
+++ src/lib/components/common/Image.svelte
@@ -5,23 +5,27 @@
 	export let src = '';
 	export let alt = '';
 
-	export let className = ' w-full outline-hidden focus:outline-hidden';
-	export let imageClassName = 'rounded-lg';
+	export let className = ' w-full outline-hidden opacity-80 scale-85 hover:scale-125 hover:opacity-100';
+	export let imageClassName = 'rounded-lg mx-auto';
 
 	let _src = '';
 	$: _src = src.startsWith('/') ? `${WEBUI_BASE_URL}${src}` : src;
+
+	let _caption = '';
+	$: _caption = alt;
 
 	let showImagePreview = false;
 </script>
 
 <button
-	class={className}
+	class={`text-center ${className}`}
 	on:click={() => {
 		showImagePreview = false;
 	}}
 	type="button"
 >
 	<img src={_src} {alt} class={imageClassName} draggable="false" data-cy="image" />
+	{@html _caption}
 </button>
 
 <ImagePreview bind:show={showImagePreview} src={_src} {alt} />
