6311ce2d97ee4c111a117886c73b095c38b97fc5
shadowfax <theshadowfaxproject@gmail.com>
2025-04-08 00:33:35 -0700

    open-webui-73 Options/Choices action buttons in UI

---
+++ src/lib/components/common/OptionGroup.svelte
@@ -0,0 +1,72 @@
+<!-- OptionGroup.svelte -->
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    export let options = [
+        { title: 'Option 1', description: 'Description for option 1', state: 'unselected'},
+        { title: 'Option 2', description: 'Description for option 2', state: 'unselected'},
+        { title: 'Option 3', description: 'Description for option 3', state: 'unselected'}
+    ];
+    export let optionSelected = false
+    export let selectedTitle = ''
+
+    $: _optionSelected = optionSelected;
+
+    function handleClick(option: { title: string, description: string, state: string }) {
+        if (_optionSelected) {
+            return;
+        } else {
+            optionSelected = true;
+            selectedTitle = option.title;
+            //console.info('OptionGroup Button clicked:', option)
+            dispatch('click', option);
+        }
+    }
+</script>
+
+
+<div class="option-group">
+    {#each options as option}
+        <div class="max-w-[max-content] flex items-center">
+            <button class="{(_optionSelected && selectedTitle === option.title)  ? 'pill-button selected' : _optionSelected ? 'pill-button disabled' : 'pill-button'} rounded-3xl px-5 py-0 flex items-center"
+                on:click={() => handleClick(option)}
+            >
+                {option.title}
+            </button>
+            <span class="description">{option.description}</span>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .option-group {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+    .description {
+        margin-left: 0.5rem;
+    }
+    .pill-button {
+        background-color: #EB8486;
+        transition: background-color 0.2s ease;
+        color: white;
+        scale: 95%;
+        cursor: pointer;
+    }
+    .pill-button:hover {
+        background-color: #EB5352;
+        scale: 105%;
+    }
+    .pill-button.selected {
+        background-color: #EB5352;
+        scale: 95%;
+        cursor: default;
+    }
+    .pill-button.disabled {
+        background-color: #cfcfcf;
+        scale: 95%;
+        cursor: default;
+    }
+</style>
