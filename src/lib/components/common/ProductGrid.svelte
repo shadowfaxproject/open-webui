ede88ab22074b88174ef169c3b12fea6bfeae692
Jitendra Borse <jitu@Jitendras-MBP.attlocal.net>
2025-05-01 15:52:21 -0700

    [Product Carousel] Intermediate commit to add product carousel layover when user selects a product. Needs cleanup

--- src/lib/components/common/ProductGrid.svelte
+++ src/lib/components/common/ProductGrid.svelte
@@ -30,25 +30,64 @@
     export let product_context = { header_message: '', footer_message: '' };
     export let productSelected = false;
     export let selectedTitle = '';
+    export let productUrl = '';
     export let showProductDetailsModal = false; // New state variable to manage modal
     export let selectedProductImages: string[] = [];
     export let generic_description = "This is placeholder text for the product description. It should be replaced with actual product information. Will span 2 lines";
+    export let currentImageIndex = 0;
+    export let touchStartX = 0;
+    export let touchEndX = 0;
+
 
     $: _productSelected = productSelected;
 
     function handleClick(product: { name: string, description: string, image: string, state: string }) {
-    if (_productSelected || product.state === 'selected' || product.state === 'disabled') {
-        return;
-    } else {
-        productSelected = true;
-        selectedTitle = product.name;
-        product.state = 'selected';
-        showProductDetailsModal = true; // Show the modal
-        selectedProductImages[0] = product.image;
-        selectedProductImages[1] = product.image;
-        dispatch('click', product);
+        console.log('##### handleClick', product);
+        if (_productSelected || product.state === 'selected' || product.state === 'disabled') {
+            return;
+        } else {
+            productSelected = true;
+            selectedTitle = product.name;
+            productUrl = product.url;
+            product.state = 'selected';
+            showProductDetailsModal = true; // Show the modal
+            selectedProductImages[0] = "/image_cache/favicon.png";
+            selectedProductImages[1] = "/image_cache/trial_box_icon.png";
+            selectedProductImages[2] = "/image_cache/favicon.png";
+            selectedProductImages[3] = "/image_cache/trial_box_icon.png";
+            // JITU : Do we want to send this dispatch?
+            // dispatch('click', product);
+        }
     }
-}
+
+    function showNextImage() {
+        console.log('##### showNextImage', currentImageIndex);
+        currentImageIndex = (currentImageIndex + 1) % selectedProductImages.length;
+        console.log('##### UPDATED showNextImage', currentImageIndex);
+    }
+
+    function showPreviousImage() {
+        console.log('##### showPreviousImage', currentImageIndex);
+        currentImageIndex = (currentImageIndex - 1 + selectedProductImages.length) % selectedProductImages.length;
+        console.log('##### UPDATED showPreviousImage', currentImageIndex);
+    }
+
+
+    function handleTouchStart(event: TouchEvent) {
+        touchStartX = event.touches[0].clientX;
+    }
+
+    function handleTouchMove(event: TouchEvent) {
+        touchEndX = event.touches[0].clientX;
+    }
+
+    function handleTouchEnd() {
+        if (touchStartX - touchEndX > 50) {
+            showNextImage();
+        } else if (touchEndX - touchStartX > 50) {
+            showPreviousImage();
+        }
+    }
 </script>
 
 {#if showProductDetailsModal}
@@ -56,13 +95,24 @@
         <div class="carousel-container">
             <button class="close-button" on:click={() => (showProductDetailsModal = false)}>Close</button>
             <!-- Carousel -->
-            {#each selectedProductImages as image}
-                <img src={image} alt="Product Image" class="carousel-image" />
-            {/each}
-            <button class="url-button" on:click={() => window.open(product.url, '_blank')}>Go to Product</button>
+            <div class="carousel" on:touchstart={handleTouchStart} on:touchmove={handleTouchMove} on:touchend={handleTouchEnd}>
+                <button class="carousel-button prev" on:click={() => showPreviousImage()}>❮</button>
+                <img src={selectedProductImages[currentImageIndex]} alt="Product Image" class="carousel-image" />
+                <button class="carousel-button next" on:click={() => showNextImage()}>❯</button>
+            </div>
+            <div class="dots">
+                {#each selectedProductImages as _, index}
+                    <span
+                        class="dot {index === currentImageIndex ? 'active' : ''}"
+                        on:click={() => (currentImageIndex = index)}
+                    ></span>
+                {/each}
+            </div>
+            <button class="url-button" on:click={() => window.open(productUrl, '_blank')}>Buy</button>
         </div>
     </div>
 {/if}
+
 
 <div class="product-grid-container">
     <!-- Add text on top -->
@@ -201,47 +251,109 @@
     }
 
     .modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.8);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1000;
-}
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.8);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+    }
 
-.carousel-container {
-    position: relative;
-    background: white;
-    padding: 1rem;
-    border-radius: 8px;
-    max-width: 90%;
-    max-height: 90%;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+    .carousel-container {
+        position: relative;
+        height: 320px;
+        background: white;
+        padding: 1rem;
+        border-radius: 8px;
+        max-width: 300px;
+        max-height: 300px;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
-.carousel-image {
-    max-width: 100%;
-    max-height: 80vh;
-    margin-bottom: 1rem;
-}
+    .close-button {
+        position: absolute;
+        top: 1rem;
+        background: red;
+        color: white;
+        border: none;
+        border-radius: 20px;
+        width: 5rem;
+        height: 2rem;
+        cursor: pointer;
+    }
 
-.close-button {
-    position: absolute;
-    top: 1rem;
-    right: 1rem;
-    background: red;
-    color: white;
-    border: none;
-    border-radius: 50%;
-    width: 2rem;
-    height: 2rem;
-    cursor: pointer;
-}
+    .carousel {
+        position: relative;
+        top: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .carousel-image {
+        max-width: 200px;
+        max-height: 200px;
+        margin-bottom: 1rem;
+    }
+
+    .url-button {
+        position: absolute;
+        bottom: 1rem;
+        background: red;
+        color: white;
+        border: none;
+        border-radius: 20px;
+        width: 5rem;
+        height: 2rem;
+        cursor: pointer;
+    }
+
+
+    .carousel-button {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        background: rgba(0, 0, 0, 0.5);
+        color: white;
+        border: none;
+        border-radius: 50%;
+        width: 2rem;
+        height: 2rem;
+        cursor: pointer;
+        z-index: 10;
+    }
+
+    .carousel-button.prev {
+        left: 1rem;
+    }
+
+    .carousel-button.next {
+        right: 1rem;
+    }
+
+    .dots {
+        display: flex;
+        justify-content: center;
+        margin-top: 1rem;
+    }
+
+    .dot {
+        width: 10px;
+        height: 10px;
+        margin: 0 5px;
+        background-color: #ccc;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+
+    .dot.active {
+        background-color: #eb5352;
+    }
 </style>
