accea55dffcec45cf10911879d9fc2a11e0dd55a
shadowfax <theshadowfaxproject@gmail.com>
2025-05-02 11:34:18 -0700

    open-webui-93 product grid implementation

--- src/lib/components/common/ProductGrid.svelte
+++ src/lib/components/common/ProductGrid.svelte
@@ -36,25 +36,36 @@
 
     $: _productSelected = productSelected;
 
-    function handleClick(product: { name: string, description: string, image: string, state: string }) {
-    if (_productSelected || product.state === 'selected' || product.state === 'disabled') {
+    function handleViewDetailsClick(product: { name: string, description: string, image: string, state: string }) {
+      if (product.state === 'selected' || product.state === 'disabled') {
         return;
-    } else {
+      } else {
         productSelected = true;
         selectedTitle = product.name;
         product.state = 'selected';
         showProductDetailsModal = true; // Show the modal
         selectedProductImages[0] = product.image;
         selectedProductImages[1] = product.image;
-        dispatch('click', product);
+      }
     }
-}
+
+    function handleCloseClick() {
+        showProductDetailsModal = false;
+        const selectedProduct = products.find(p => p.state === 'selected');
+        if (selectedProduct) {
+            selectedProduct.state = 'unselected';
+            productSelected = false;
+            selectedTitle = '';
+            selectedProductImages = [];
+        }
+    }
 </script>
 
 {#if showProductDetailsModal}
     <div class="modal">
         <div class="carousel-container">
-            <button class="close-button" on:click={() => (showProductDetailsModal = false)}>Close</button>
+<!--            <button class="close-button" on:click={() => (showProductDetailsModal = false)}>Close</button>-->
+            <button class="close-button" on:click={() => handleCloseClick()} >Close</button>
             <!-- Carousel -->
             {#each selectedProductImages as image}
                 <img src={image} alt="Product Image" class="carousel-image" />
@@ -83,12 +94,25 @@
                 <h3 class="grid-item-name">{product.name}</h3>
 
                 <p class="grid-item-price">{product.price}</p>
+
                 <button
-                    class="{(_productSelected && selectedTitle === product.name) || (product.state === 'selected') ? 'pill-button selected' : (_productSelected || product.state === 'disabled') ? 'pill-button disabled' : 'pill-button'}"
-                    on:click={() => handleClick(product)}
-                    >
-                    View Details
+                class="absolute top-0 right-0 flex cursor-pointer px-1 py-1 rounded-xl hover:bg-gray-50 dark:hover:bg-gray-850 transition"
+                on:click={() => handleViewDetailsClick(product)}
+                >
+                    <div class=" m-auto self-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" id="magnifying-glass">
+                          <path fill="#333" d="M9.37395 6.4078C8.5036 6.44165 7.71765 6.75215 7.2972 7.1727C7.10195 7.368 6.78535 7.368 6.5901 7.17275C6.3948 6.97755 6.39475 6.66095 6.58995 6.46565C7.2387 5.81685 8.2944 5.44905 9.33505 5.40855C10.3825 5.3678 11.5261 5.6546 12.3269 6.4556C12.5221 6.65085 12.5221 6.96745 12.3268 7.1627C12.1316 7.35795 11.815 7.3579 11.6197 7.16265C11.0742 6.617 10.2375 6.3742 9.37395 6.4078Z"></path>
+                          <path fill="#333" fill-rule="evenodd" d="M14.4678 13.692C15.4238 12.5604 16 11.0974 16 9.5C16 5.91015 13.0898 3 9.5 3C5.91015 3 3 5.91015 3 9.5C3 13.0898 5.91015 16 9.5 16C11.0974 16 12.5604 15.4238 13.692 14.4678L13.5 15.5721L17.2568 19.3289L19.3289 17.2568L15.5721 13.5L14.4678 13.692ZM9.5 14.5C12.2614 14.5 14.5 12.2614 14.5 9.5C14.5 6.73858 12.2614 4.5 9.5 4.5C6.73858 4.5 4.5 6.73858 4.5 9.5C4.5 12.2614 6.73858 14.5 9.5 14.5Z" clip-rule="evenodd"></path>
+                          <path fill="#333" d="M17.9639 20.0361L20.0361 17.9639L20.7139 18.6418C21.0954 19.0233 21.0954 19.6418 20.7139 20.0233L20.0233 20.7139C19.6418 21.0954 19.0233 21.0954 18.6418 20.7139L17.9639 20.0361Z"></path>
+                        </svg>
+                    </div>
                 </button>
+<!--                <button-->
+<!--                    class="{(product.state === 'selected') ? 'pill-button selected' : 'pill-button'}"-->
+<!--                    on:click={() => handleViewDetailsClick(product)}-->
+<!--                    >-->
+<!--                    Details-->
+<!--                </button>-->
             </div>
         {/each}
     </div>
@@ -118,6 +142,7 @@
     }
 
     .grid-item {
+        position: relative;
         display: flex;
         flex-direction: column;
         text-align: left;
