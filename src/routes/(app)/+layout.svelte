e4498eeadacdf062627644a2966c2c19d75bc1c5
shadowfax <theshadowfaxproject@gmail.com>
2025-09-04 02:24:58 -0700

    open-webui-3 Remove technical pop-up from first visit.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -227,9 +227,8 @@
 				}
 			});
 
-			if ($user?.role === 'admin' && ($settings?.showChangelog ?? true)) {
-				showChangelog.set($settings?.version !== $config.version);
-			}
+			// Do not show changelog for any user
+ 			showChangelog.set(false);
 
 			if ($user?.role === 'admin' || ($user?.permissions?.chat?.temporary ?? true)) {
 				if ($page.url.searchParams.get('temporary-chat') === 'true') {
